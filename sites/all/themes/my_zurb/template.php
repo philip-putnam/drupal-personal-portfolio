3b621870d0d3d2b7cc493dc4e0c4d522247e5c5c
Philip Putnam <putnamp989@gmail.com>
2017-04-24 11:51:45 -0700

    replace incorrect database, add zurb sub theme

---
+++ sites/all/themes/my_zurb/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function my_zurb_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function my_zurb_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function my_zurb_preprocess_node(&$variables) {
+}
